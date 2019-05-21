c09ec4a1c7d890d6bbb1c6ab8cbb025a6f85a2b2
Ivan Popov <popov4ivan@gmail.com>
2019-05-21 17:25:26 +0300

    fix Even.php input

--- src/Even.php
+++ src/Even.php
@@ -11,6 +11,7 @@ function run()
     line('Answer "yes" if number even otherwise answer "no"');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
+    $counter = 0;
     for ($i = 1; $i <= 3; $i++) {
         $number = rand(1, 100);
         line("Question: %s", $number);
@@ -18,11 +19,12 @@ function run()
         ($number % 2 === 0) ? $corrAnswer = 'yes' : $corrAnswer = 'no';
         if ($answer === $corrAnswer) {
             line('Correct!');
+            $counter++;
         } else {
             line('"%s" is wrong answer ;(. Correct answer was "%s"', $answer, $corrAnswer);
             line("Let's try again, %s!", $name);
             break;
         }
-        line("Congratulations, %s!", $name);
     }
+    if ($counter === 3) line("Congratulations, %s!", $name);
 }
