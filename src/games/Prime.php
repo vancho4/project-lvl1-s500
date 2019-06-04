8102f13fc54033b287f2d85a0123bbb9ac7ac41c
Ivan <popov4ivan@gmail.com>
2019-06-04 17:29:32 +0300

    fix Prime.php

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -10,6 +10,9 @@ const TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function isPrime($number)
 {
+    if ($number === 1) {
+        return false;
+    }
     for ($i = 2; $i <= $number / 2; $i++) {
         if ($number % $i === 0) {
             return false;
